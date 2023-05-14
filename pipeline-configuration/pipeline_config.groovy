c5c86a72ec60bf4c440d2ad6169cbaede509570d
vijay <vijay@devopzlabs.com>
2023-05-14 03:05:35 -0500

    update

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -2,21 +2,13 @@
   	sonar
 }
 
-def stageProps = [
-	"data" : "init"
-]
 
-if(buildType == "full"){
     stages{
         continuous_integration{
             build(stageProps)
             static_build(stageProps)
         }
-    }
-}
-elseif(buildType == "buildOnly"){
-    stages{
-        continuous_integration{
+        ci_minimal{
             build(stageProps)
         }
     }
