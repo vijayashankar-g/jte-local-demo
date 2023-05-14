5d23992359e2b278ae009d4fee4426f9bf4fc6a6
vijay <vijay@devopzlabs.com>
2023-05-14 02:32:38 -0500

    update

---
+++ libraries/gradel/steps/build.groovy
@@ -0,0 +1,9 @@
+void call(stageProps){
+    stage("Maven: Build"){
+        println "build from the maven library"
+        println stageProps.data
+        println stageProps.stageName
+        stageProps.stageName = "Maven"
+        println appType
+    }
+}
