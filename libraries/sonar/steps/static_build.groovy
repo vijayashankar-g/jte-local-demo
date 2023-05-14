64a1c9c512f4b373d08bf2620292cd5dfb54d0b9
vijay <vijay@devopzlabs.com>
2023-05-14 02:21:33 -0500

    update

--- libraries/sonar/steps/static_build.groovy
+++ libraries/sonar/steps/static_build.groovy
@@ -2,6 +2,7 @@ void call(stageProps){
     stage("Static Code Analysis"){
         println "static code analysis"
         println "$stageProps.data"
-
+        println "$stageProps.stageName"
+        stageProps.stageName = "Maven"
     }
 }
