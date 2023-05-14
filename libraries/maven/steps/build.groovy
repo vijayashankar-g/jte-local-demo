43c1c43dec82e5dfddcd2bb977b3d868b73b6c21
vijay <vijay@devopzlabs.com>
2023-05-14 02:20:46 -0500

    update

--- libraries/maven/steps/build.groovy
+++ libraries/maven/steps/build.groovy
@@ -1,6 +1,9 @@
 void call(stageProps){
     stage("Maven: Build"){
         println "build from the maven library"
-        println "{stageProps.data}"
+        println "$stageProps.data"
+        stageProps.stageName = "Maven"
+        println "$stageProps.stageName"
+
     }
 }
