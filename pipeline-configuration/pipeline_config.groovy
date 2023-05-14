8c889506580bfc2bbe4dadf2e6a4f491d111beb1
vijay <vijay@devopzlabs.com>
2023-05-14 03:18:43 -0500

    update

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,14 +1,3 @@
 @merge libraries{
   	sonar
 }
-
-
-// stages{
-//     continuous_integration(stageProps){
-//         build
-//         static_build
-//     }
-//     ci_minimal(stageProps){
-//         build
-//     }
-// }
